ead952eb2a3e1f650db266f0cfeeaffc1514b886
PL(I)-C-programming-Savali- <patilsavali08@gmail.com>
2026-09-21 00:04:29 +0530

    Add files via upload

---
+++ srp11.c
@@ -0,0 +1,26 @@
+int num;
+printf("Enter your number: ");
+scanf("%d", &num);
+
+if (num % 2 == 0)
+{
+
+}
+else
+{
+printf("The number is Odd.");
+
+return 0;
+
+// Program (11) -> Write a program to check whether a g
+
+#include <stdio.h>
+
+int main()
+{
+
+printf("The number is Even.");
+
+}
+
+}
